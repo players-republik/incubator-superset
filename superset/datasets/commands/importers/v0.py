2be6e6f4a610a146542b6d0317915aceb764e166
Beto Dealmeida <roberto@dealmeida.net>
2020-12-02 19:30:27 -0800

    Fix test

--- superset/datasets/commands/importers/v0.py
+++ superset/datasets/commands/importers/v0.py
@@ -285,20 +285,15 @@ class ImportDatasetsCommand(BaseCommand):
 
     # pylint: disable=unused-argument
     def __init__(
-        self,
-        *args: Any,
-        contents: Dict[str, str],
-        sync_columns: bool = False,
-        sync_metrics: bool = False,
-        **kwargs: Any,
+        self, contents: Dict[str, str], *args: Any, **kwargs: Any,
     ):
         self.contents = contents
         self._configs: Dict[str, Any] = {}
 
         self.sync = []
-        if sync_columns:
+        if kwargs.get("sync_columns"):
             self.sync.append("columns")
-        if sync_metrics:
+        if kwargs.get("sync_metrics"):
             self.sync.append("metrics")
 
     def run(self) -> None:
